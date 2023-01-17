8acc88d16c45ac3196bf96b9ed1d642190bbed30
Arik Bidny <arikbidny@gmail.com>
2023-01-17 13:43:08 +0200

    Adding crud operations react app

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-      <App />
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
